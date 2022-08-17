dd70d5d7b222219d2ae23a37bc05a7dc753bda9c
sibergold <kaynakca21@hotmail.com>
2022-08-17 18:38:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace while_dongusu{
+    class Program{
+        static void Main(string[] args){
+            //int sayi=Convert.ToInt32(Console.ReadLine());
+            //int sayac=1;
+            //int toplam=0;
+            //while(sayac<=sayi){
+              //toplam+=sayac;
+              //sayac++;
+            //}
+            //Console.WriteLine(toplam/sayi);
+            //char character='a';
+            //while(character<'z'){
+            //character ++;}
+            Console.WriteLine("**** Foreach ****");
+            string[] arabalar= {"BMW","Ford","Toyota","Nissan"};
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+            
+            
+            }
+    }
+}
